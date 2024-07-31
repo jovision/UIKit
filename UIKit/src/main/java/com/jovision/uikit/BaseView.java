22952e9bbf4f557ddf9ed04897ba0fab0daf661b
yuyunlong <yuyunlong@jovision.com>
2024-07-31 16:49:46 +0800

    增加BaseView

---
+++ UIKit/src/main/java/com/jovision/uikit/BaseView.java
@@ -0,0 +1,15 @@
+package com.jovision.uikit;
+
+/**
+ * Created by ZHP on 2017/6/24.
+ */
+
+public interface BaseView {
+
+    void showLoading();
+
+    void showError(String msg);
+
+    void dismissLoading();
+
+}
