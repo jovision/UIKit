58cccfeb5c1eb96ae9f2846d6f42412115a5f7a0
yuyunlong <yuyunlong@jovision.com>
2024-08-01 16:24:36 +0800

    增加alertdialog控件 NetKit组件使用到了

--- app/src/main/java/com/jovision/uikit/MainActivity.java
+++ app/src/main/java/com/jovision/uikit/MainActivity.java
@@ -3,6 +3,9 @@ package com.jovision.uikit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.jovision.uikit.widget.sweetdialog.SweetAlertDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.dialog).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                SweetAlertDialog sd = new SweetAlertDialog(MainActivity.this);
+                sd.setCancelable(true);
+                sd.setCanceledOnTouchOutside(true);
+                sd.show();
+            }
+        });
     }
 }
