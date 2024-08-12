cf38cf59f47dbb554469e5d77efdb76cc555f912
yuyunlong <yuyunlong@jovision.com>
2024-08-12 19:46:44 +0800

    封装底部toast

--- app/src/main/java/com/jovision/uikit/MainActivity.java
+++ app/src/main/java/com/jovision/uikit/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ToastKit.showBottomToastSafe(MainActivity.this, "底部toast");
+
             }
         });
     }
